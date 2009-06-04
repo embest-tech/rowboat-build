7a929f177d4dd829a9ce296a517cbed9e509faa9
Matthew Ranostay <mranostay@embeddedalley.com>
2009-06-04 11:37:30 -0400

    Add EA mk files for our custom builds. This removes the Phone service and adds the Email one.
    Signed-off-by: Matthew Ranostay <mranostay@embeddedalley.com>

---
+++ target/product/ea-core.mk
@@ -0,0 +1,22 @@
+PRODUCT_BRAND :=
+PRODUCT_NAME :=
+PRODUCT_DEVICE :=
+PRODUCT_POLICY := android.policy_phone
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.config.notification_sound=F1_New_SMS.ogg
+
+PRODUCT_PACKAGES := \
+    framework-res \
+    Browser \
+    Contacts \
+    Launcher \
+    HTMLViewer \
+    ContactsProvider \
+    DownloadProvider \
+    GoogleSearch \
+    MediaProvider \
+    SettingsProvider \
+    UserDictionaryProvider \
+    PackageInstaller \
+    Bugreport
+
