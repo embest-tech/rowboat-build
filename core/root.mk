622bdb6f189934759232a372a18cc3c8bcf953fe
Yegor Yelizarov <yegor_yelizarov@mentor.com>
2010-10-21 20:24:09 +0400

    Fix continuous building
    Change-Id: Ibaab9167d52726426d81cc0c73db9f32b2e25bca

--- core/root.mk
+++ core/root.mk
@@ -41,7 +41,7 @@ ifeq ($(strip $(dvsdk_not_installed)),)
 	make -C external/ti-dsp combo_target=$(combo_target) $(combo_target)TOOLS_PREFIX=$($(combo_target)TOOLS_PREFIX) HOST_PREBUILT_TAG=$(HOST_PREBUILT_TAG) clean
 endif
 	make -C external/ti-dsp combo_target=$(combo_target) $(combo_target)TOOLS_PREFIX=$($(combo_target)TOOLS_PREFIX) HOST_PREBUILT_TAG=$(HOST_PREBUILT_TAG)
-	make
+	make droid
 
 dvsdk_clean:
 	make -C external/ti-dsp combo_target=$(combo_target) $(combo_target)TOOLS_PREFIX=$($(combo_target)TOOLS_PREFIX) HOST_PREBUILT_TAG=$(HOST_PREBUILT_TAG) clean
